c9f202482d043dd545f64f359a702ee172bf06e1
zaheer ali <zaheerrahman0@gmail.com>
2024-08-29 21:50:43 -0700

    Project 3

--- 3/script.js
+++ 3/script.js
@@ -2,24 +2,31 @@ const start = document.getElementById("start");
 const stop = document.getElementById("stop");
 let body = document.body;
 
-function bg_Color() {
-    let hex = "0123456789ABCDEF";
-    let color = "#";
-    for (let i = 0; i < 6; i++){
-        color += hex[Math.floor(Math.random() * 16)]
-    }
+let intervalId;
 
+function bg_Color() {
+  let hex = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += hex[Math.floor(Math.random() * 16)];
+  }
     return color;
 }
 
-start.addEventListener("click", ()=>{
-    setInterval(()=> {
-        body.style.backgroundColor = bg_Color();
-    }, 1000)
-})
+function Start() {
+  if (!intervalId) {
+    intervalId = setInterval(() => {
+      body.style.backgroundColor = bg_Color();
+    }, 1000);    
+  }
+}
 
-stop.addEventListener("click", ()=> {
-    clearInterval(()=> {
-        bg_Color()
-    })
-})
+start.addEventListener("click", () => {
+  Start();
+});
+
+stop.addEventListener("click", () => {
+  clearInterval(intervalId);
+  intervalId = null;
+});
+
